3a0603c3b89febc7c3ff2928ed2e50184ca92815
MickLarch <mickael.larcheveque@gmail.com>
2015-10-22 12:55:40 +0200

    Crion du serveur, de la session et de la todolist[]

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ var ejs = require('ejs');
 var session = require('cookie-session');
 var bodyparser = require('body-parser');
 var urlencodedparser = bodyparser.urlencoded({extended: false});
+
+var app = express();
+
+//Création d'une session
+app.use(session({secret: 'todolistsecured'}))
+
+//Initialisation de la session si nécessaire
+.use(function(req, res, next) {
+    if (typeof(req.session.todolist) == undefined){
+        req.session.todolist = [];
+    }
+    next();
+})
