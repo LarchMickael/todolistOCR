7fadbdc9f9808e59307df97fb0e806a8f12cb702
MickLarch <mickael.larcheveque@gmail.com>
2015-10-22 14:49:43 +0200

    Suppression de l'appel inutile au module ejs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var ejs = require('ejs');
 var session = require('cookie-session');
 var bodyparser = require('body-parser');
 var urlencodedparser = bodyparser.urlencoded({extended: false});
